b23d51376c4473ef93924d70ed00a9f5dfebf750
Josh <joshua.hansen188@gmail.com>
2018-08-01 21:52:02 +1000

    Started bluetooth module

---
+++ bluetooth_notification.py
@@ -0,0 +1,9 @@
+import bluetooth
+
+
+def findDevices():
+    nearby_devices = bluetooth.discover_devices()
+    for device in nearby_devices:
+        print("Name: {0} \nDevice: {1}".format(bluetooth.lookup_name(device), device))
+
+findDevices()
